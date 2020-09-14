9132800758caf74169e7775c9766bc63f9668297
Irene <irenesis123@gmail.com>
2020-09-14 03:30:30 -0400

    [it] large desktop contact form styles added

--- pages/contact.js
+++ pages/contact.js
@@ -9,29 +9,28 @@ export default function Contact() {
       </Head>
 
       <div className="contact-hero"></div>
-      <section class="grey-background">
-        <div className="white-contact-card wrapper">
-          <h2>Contact</h2>
-          <p>
-            Feel free to fill out the form below/contact me if:
-            <ol>
-              <li>You'd like to work together.</li>
-              <li>You'd like to hire me.</li>
-              <li>You want to discuss a freelance project.</li>
-              <li>
-                You want to talk about Pokémon GO and schedule 5-Star raids with
-                me.
-              </li>
-              <li>
-                You'd like to discuss your favourite personality from Netflix's
-                Selling Sunset.
-              </li>
-              <li>
-                I'd like to think I'm friendly, so in general, feel free to get
-                in touch!
-              </li>
-            </ol>
-          </p>
+      <section class="grey-background contact-card-container">
+        <div className="white-contact-card ">
+          <h1>Contact</h1>
+          <p>Feel free to contact me if:</p>
+          <ol>
+            <li>You'd like to work together.</li>
+            <li>You'd like to hire me.</li>
+            <li>You'd like to discuss a freelance project.</li>
+            <li>
+              You'd like to talk about Pokémon GO and schedule 5-Star raids with
+              me.
+            </li>
+            <li>
+              You'd like to discuss your favourite personality from Netflix's
+              Selling Sunset.
+            </li>
+            <li>
+              I'd like to think I'm friendly, so in general, feel free to get in
+              touch!
+            </li>
+          </ol>
+
           <form
             action="https://formspree.io/itruong25.it@gmail.com"
             method="POST"
@@ -44,44 +43,30 @@ export default function Contact() {
                 to get in touch.
               </legend>
               <div class="personal-input">
-                <label for="name" class="visuallyHidden">
-                  {" "}
-                  Name
-                </label>
-                <input
-                  type="name"
-                  placeholder="Name"
-                  required="true"
-                  id="name"
-                  name="name"
-                />
+                <label for="name"> Name</label>
+                <input type="name" required="true" id="name" name="name" />
 
-                <label for="email-address" class="visuallyHidden">
-                  Email
-                </label>
+                <label for="email-address">Email</label>
                 <input
                   type="email"
-                  placeholder="Email address"
                   required="true"
                   id="email-address"
                   name="_replyto"
                 />
+                <label for="message">Message</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  required="true"
+                  cols="30"
+                  rows="10"
+                  name="message"
+                ></textarea>
               </div>
 
-              <label for="message" class="visuallyHidden">
-                Message
-              </label>
-              <textarea
-                name="message"
-                placeholder="Message"
-                id="message"
-                required="true"
-                cols="30"
-                rows="10"
-                name="message"
-              ></textarea>
-
-              <button type="submit">Submit</button>
+              <button className="btn-primary form-submit" type="submit">
+                Submit
+              </button>
             </fieldset>
           </form>
         </div>
